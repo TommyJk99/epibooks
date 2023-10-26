0cdbf00e2abc3c38bcda52b431555f64741470d1
Thomas Mazzotta <thomasmazzotta99@gmail.com>
2023-10-26 22:48:36 +0200

    Revert "added red border on click"
    This reverts commit 4e0f9074d94a4a4c88e0c9be0e2e804a0c50c89b.

--- src/SingleBook.jsx
+++ src/SingleBook.jsx
@@ -1,20 +1,8 @@
 import { Card } from "react-bootstrap";
-import React, { useState } from "react";
 
-//pongo lo stato iniziale
 const SingleBook = ({ book }) => {
-  const [selected, setSelected] = useState(false);
-
-  //al click della card cambio lo stato di selected
-  const handleClick = () => {
-    setSelected(!selected);
-  };
-
-  //se selected è tr allora inserisco il css all'interno della card quando aggiorno
-  const cardBorder = selected ? { border: "2px solid red", width: "18rem" } : { width: "18rem" };
-
   return (
-    <Card style={cardBorder} onClick={handleClick}>
+    <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={book.img} />
       <Card.Body>
         <Card.Title className="text-center fs-5">{book.title}</Card.Title>
